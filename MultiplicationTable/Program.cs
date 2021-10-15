143811e3e961ec4ad25343a8267f13e390e09b87
Sam Wu <swu@nait.ca>
2021-10-15 08:03:34 -0600

    Oct 14 update

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,77 @@
+﻿/*  Purpose:    Display a multiple table of the numbers 1 to 9
+ *  
+ *  Inputs:     N/A
+ *  
+ *  Outputs:    A multiplication table for the numbers 1 to 9
+ *  
+ *  Algorithm:  1) Display a welcome message
+ *              2) For num2 ranging from 1 through 9 do the following:
+ *                      Display num2 follow by 3 spaces
+ *                 End For
+ *                 Display a newline
+ *                 
+ *                 For num1 ranging from 1 through 9 do the following:
+ *                      Display num1 follow by 3 spaces
+ *                      For num2 ranging from 1 through 9 do the following:
+ *                          Calculate 
+ *                              multiplicationResult = num1 * num2
+ *                          Display multiplicationResult follow 3 spaces
+ *                      End For
+ *                      Display a new line
+ *                 End For
+ *                 
+ * */
+using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display a welcome message
+            Console.WriteLine("This program displays a Multiplication Table of the numbers 1 through 9.");
+            /*
+             *     For num2 ranging from 1 through 9 do the following:
+ *                      Display num2 follow by 3 spaces
+ *                 End For
+ *                 Display a newline
+ *                 
+ *                 For num1 ranging from 1 through 9 do the following:
+ *                      Display num1 follow by 3 spaces
+ *                      For num2 ranging from 1 through 9 do the following:
+ *                          Calculate 
+ *                              multiplicationResult = num1 * num2
+ *                          Display multiplicationResult follow 3 spaces
+ *                      End For
+ *                      Display a new line
+ *                 End For
+             */
+            for (int num2 = 1; num2 <= 9; num2++ )
+            {
+                if (num2 == 1)
+                {
+                    Console.Write($"{num2,8}");
+                }
+                else
+                {
+                    Console.Write($"{num2,4}");
+                }
+                
+            }
+            Console.WriteLine();
+
+            for (int num1 = 1; num1 <= 9; num1++)
+            {
+                Console.Write($"{num1,-4}");
+                for (int num2 = 1; num2 <= 9; num2++)
+                {
+                    double multiplicationResult = num1 * num2;
+                    Console.Write($"{multiplicationResult,4}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ RectangleFilledPattern/Program.cs
@@ -0,0 +1,63 @@
+﻿/*  Purpose:    Draw a rectangle box with a user specified number of rows and columns using 
+ *              a user specified filled character.
+ *              
+ *  Inputs:     rows
+ *              columns
+ *              filledCharacter
+ * 
+ *  Output:     A rectangle box draw on the screen with filledCharacter of rows and columns
+ * 
+ *  Algorithm:  1) Display a welcome message
+ *              2) Prompt and read in the the numbers of rows and columns and filledCharacter
+ *              3) For row ranging from 1 through rows do the following:
+ *                      For column ranging from 1 through columns do the following:
+ *                          Display a single filledCharacter
+ *                      End For
+ *                      Display a new line
+ *                 End For
+ * */
+using System;
+
+namespace RectangleFilledPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables for inputs 
+            int rows,       // number of rows to display
+                columns;    // number of columns for each row to display
+            char filledChar;    // the character used to print the rectangle
+
+            // Display a welcome message
+            Console.WriteLine("This program display rectangle on the screen");
+            // Prompt and read in the the numbers of rows and columns and filledCharacter
+            Console.Write("Enter the number of rows to print: ");
+            rows = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of columns to print: ");
+            columns = int.Parse(Console.ReadLine());
+            Console.Write("Enter the character used to print the rectange: ");
+            //filledChar = char.Parse(Console.ReadLine());
+            filledChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            /*  For row ranging from 1 through rows do the following:
+ *                      For column ranging from 1 through columns do the following:
+ *                          Display a single filledCharacter
+ *                      End For
+ *                      Display a new line
+ *                 End For
+             * */
+            for (int currentRow = 1; currentRow <= rows; currentRow++)
+            {
+                for (int currrentColumn = 1; currrentColumn <= columns; currrentColumn++)
+                {
+                    Console.Write(filledChar);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
